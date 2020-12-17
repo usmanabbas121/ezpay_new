915de3acce70cce8260a8cd5cca40e83ac15b937
Dhanunjay G <dhanunjay1988@github.com>
2020-12-18 04:22:32 +0530

    added Splash screen and Routing changes
    App Name changed
    Splash screen added
    App start routing is changed based on shared preferences

--- lib/login_signup_verify/signup.dart
+++ lib/login_signup_verify/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Signup extends StatefulWidget {
   @override
@@ -7,6 +8,14 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  Future<Null> saveLocalData() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isSignedIn', true);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,6 +72,7 @@ class _SignupState extends State<Signup> {
                       Padding(
                         padding: const EdgeInsets.only(left: 25, right: 25),
                         child: Form(
+                          key: _formKey,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
@@ -74,6 +84,12 @@ class _SignupState extends State<Signup> {
                                       .withOpacity(.6),
                                   fontWeight: FontWeight.w500),),
                               TextFormField(
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'Please enter Your Full Name';
+                                  }
+                                },
+                                //onSaved: (val) => setState(() => _user.name = val),
                                 style: TextStyle(
                                     fontSize: 15.0,
                                     fontFamily: "Proxmia",
@@ -122,6 +138,70 @@ class _SignupState extends State<Signup> {
                               SizedBox(height: 20,),
 
 
+
+
+                              Text("Phone Number",style: TextStyle(
+                                  fontSize: 13.0,
+                                  fontFamily: "Proxmia",
+                                  color: Theme.of(context)
+                                      .primaryColor
+                                      .withOpacity(.6),
+                                  fontWeight: FontWeight.w500),),
+                              TextFormField(
+                                keyboardType: TextInputType.number,
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'Please enter a Phone Number';
+                                  }
+                                },
+                                //onSaved: (val) => setState(() => _user.phoneNumber = val),
+                                style: TextStyle(
+                                    fontSize: 15.0,
+                                    fontFamily: "Proxmia",
+                                    color: Theme.of(context)
+                                        .primaryColor
+                                        .withOpacity(.9),
+                                    fontWeight: FontWeight.bold),
+                                decoration: InputDecoration(
+                                    alignLabelWithHint: true,
+                                    isDense: true,
+                                    prefixIconConstraints:
+                                    BoxConstraints(minHeight: 15),
+                                    prefixIcon: Padding(
+                                      padding: const EdgeInsets.only(
+                                          right: 10, bottom: 05, top: 03),
+                                      child:Container(
+                                          height: 20,
+                                          width: 20,
+                                          child: SvgPicture.asset("assets/iconnnn.svg"))
+                                    ),
+                                    floatingLabelBehavior:
+                                    FloatingLabelBehavior.always,
+                                    hintText: "Enter your phone number",
+                                    hintStyle: TextStyle(
+                                        fontSize: 15.0,
+                                        fontFamily: "Proxmia",
+                                        color: Theme.of(context)
+                                            .primaryColor
+                                            .withOpacity(.5),
+                                        fontWeight: FontWeight.w100),
+                                    enabledBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: Theme.of(context)
+                                              .primaryColor
+                                              .withOpacity(.2),width: 0.3),
+                                    ),
+                                    focusedBorder: UnderlineInputBorder(
+                                      borderSide: BorderSide(
+                                        color: Theme.of(context)
+                                            .primaryColor
+                                            .withOpacity(.5),
+                                      ),
+                                    )),
+                              ),
+                              SizedBox(height: 20,),
+
+
                               Text("Email",style: TextStyle(
                                   fontSize: 13.0,
                                   fontFamily: "Proxmia",
@@ -130,6 +210,13 @@ class _SignupState extends State<Signup> {
                                       .withOpacity(.6),
                                   fontWeight: FontWeight.w500),),
                               TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'Please enter an Email Address';
+                                  }
+                                },
+                                //onSaved: (val) => setState(() => _user.email = val),
                                 style: TextStyle(
                                     fontSize: 15.0,
                                     fontFamily: "Proxmia",
@@ -179,63 +266,6 @@ class _SignupState extends State<Signup> {
 
 
 
-                              Text("Phone Number",style: TextStyle(
-                                  fontSize: 13.0,
-                                  fontFamily: "Proxmia",
-                                  color: Theme.of(context)
-                                      .primaryColor
-                                      .withOpacity(.6),
-                                  fontWeight: FontWeight.w500),),
-                              TextFormField(
-                                style: TextStyle(
-                                    fontSize: 15.0,
-                                    fontFamily: "Proxmia",
-                                    color: Theme.of(context)
-                                        .primaryColor
-                                        .withOpacity(.9),
-                                    fontWeight: FontWeight.bold),
-                                decoration: InputDecoration(
-                                    alignLabelWithHint: true,
-                                    isDense: true,
-                                    prefixIconConstraints:
-                                    BoxConstraints(minHeight: 15),
-                                    prefixIcon: Padding(
-                                      padding: const EdgeInsets.only(
-                                          right: 10, bottom: 05, top: 03),
-                                      child:Container(
-                                          height: 20,
-                                          width: 20,
-                                          child: SvgPicture.asset("assets/iconnnn.svg"))
-                                    ),
-                                    floatingLabelBehavior:
-                                    FloatingLabelBehavior.always,
-                                    hintText: "Enter your phone number",
-                                    hintStyle: TextStyle(
-                                        fontSize: 15.0,
-                                        fontFamily: "Proxmia",
-                                        color: Theme.of(context)
-                                            .primaryColor
-                                            .withOpacity(.5),
-                                        fontWeight: FontWeight.w100),
-                                    enabledBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                          color: Theme.of(context)
-                                              .primaryColor
-                                              .withOpacity(.2),width: 0.3),
-                                    ),
-                                    focusedBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Theme.of(context)
-                                            .primaryColor
-                                            .withOpacity(.5),
-                                      ),
-                                    )),
-                              ),
-                              SizedBox(height: 20,),
-
-
-
-
                               Text("Password",style: TextStyle(
                                   fontSize: 13.0,
                                   fontFamily: "Proxmia",
@@ -245,6 +275,12 @@ class _SignupState extends State<Signup> {
                                   fontWeight: FontWeight.w500),),
                               TextFormField(
                                 obscureText: true,
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'Please enter Password';
+                                  }
+                                },
+                                //onSaved: (val) => setState(() => _user.password = val),
                                 style: TextStyle(
                                     fontSize: 15.0,
                                     fontFamily: "Proxmia",
@@ -304,6 +340,12 @@ class _SignupState extends State<Signup> {
                                   fontWeight: FontWeight.w500),),
                               TextFormField(
                                 obscureText: true,
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return 'Please Re-enter Password';
+                                  }
+                                },
+                                //onSaved: (val) => setState(() => _user.password = val),
                                 style: TextStyle(
                                     fontSize: 15.0,
                                     fontFamily: "Proxmia",
@@ -358,7 +400,15 @@ class _SignupState extends State<Signup> {
                                   shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(10.0),
                                       side: BorderSide(color: Theme.of(context).primaryColor)),
-                                  onPressed: () {},
+                                  onPressed: () {
+                                    final form = _formKey.currentState;
+                                    if (form.validate()) {
+                                      form.save();
+                                      Navigator.pop(context, "signin");
+                                      Navigator.pushReplacementNamed(context, "dashboard");
+                                      //saveLocalData(); //TODO: un-comment on release version
+                                    }
+                                  },
                                   color: Theme.of(context).primaryColor,
                                   textColor: Colors.white,
                                   child: Text(
@@ -384,8 +434,8 @@ class _SignupState extends State<Signup> {
                                           .withOpacity(.3),
                                       fontWeight: FontWeight.w100),),
                                   InkWell(
-                                    onTap: (){
-                                      Navigator.pushNamed(context, "signin");
+                                    onTap: () {
+                                      Navigator.pop(context, );
                                     },
                                     child: Text(" Sign In ",style: TextStyle(
                                         fontSize: 15.0,
